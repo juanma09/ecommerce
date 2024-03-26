adf5351b15d8b23e95e15d24be2d2224d6179cbe
juanma09 <mateoelizeche09@gmail.com>
2024-03-26 05:19:57 -0400

    Revert "second update"
    This reverts commit 48be488f21716384193fc0b3ca5212aa870fdc80.

---
+++ js/custom.js
@@ -0,0 +1,63 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+//  owl carousel script
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+//    end owl carousel script 
+
+
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+
+// Scroll 
+window.addEventListener('scroll', reveal);
+
+function reveal()
+{
+    var reveals = document.querySelectorAll('.reveal, .left-reveal')
+    
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+   
+}
